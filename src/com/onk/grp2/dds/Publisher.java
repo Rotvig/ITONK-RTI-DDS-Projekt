f38c23f9b30039d950bc0456fc8580ab3530ecd2
sune <201270842@post.au.dk>
2015-05-18 00:14:08 +0200

    Changed Publisher and Subscriber to singletons

--- src/com/onk/grp2/dds/Publisher.java
+++ src/com/onk/grp2/dds/Publisher.java
@@ -9,22 +9,31 @@ import com.rti.dds.infrastructure.InstanceHandle_t;
 import com.rti.dds.infrastructure.StatusKind;
 import com.rti.dds.topic.Topic;
 
-public class Publisher implements Closeable {
+public enum Publisher implements Closeable {
+	INSTANCE;
+
 	private DomainParticipant participant;
 	private com.rti.dds.publication.Publisher publisher;
 	private Topic topic;
 	private WindTurbineDataWriter writer;
+	private boolean initialized = false;
 
-	public Publisher(int domainId, String topicName) {
-		createParticipant(domainId);
-		createPublisher();
-		createTopic(topicName);
-		createDataWriter();
+	private Publisher() {
+
 	}
-	
+
+	public void Initialize(int domainId, String topicName) {
+		if (!initialized) {
+			createParticipant(domainId);
+			createPublisher();
+			createTopic(topicName);
+			createDataWriter();
+			initialized = true;
+		}
+	}
 
 	private void createDataWriter() {
-		writer = (WindTurbineDataWriter)publisher.create_datawriter(topic,
+		writer = (WindTurbineDataWriter) publisher.create_datawriter(topic,
 				com.rti.dds.publication.Publisher.DATAWRITER_QOS_DEFAULT, null,
 				StatusKind.STATUS_MASK_NONE);
 
@@ -52,17 +61,24 @@ public class Publisher implements Closeable {
 				StatusKind.STATUS_MASK_NONE);
 	}
 
-
 	@Override
 	public void close() throws IOException {
 		participant.delete_contained_entities();
-		DomainParticipantFactory.TheParticipantFactory.delete_participant(participant);
+		DomainParticipantFactory.TheParticipantFactory
+				.delete_participant(participant);
 		DomainParticipantFactory.finalize_instance();
-		
+
 	}
-	
+
 	public void notify(WindTurbine windTurbineStatus) {
-		InstanceHandle_t instanceHandle = InstanceHandle_t.HANDLE_NIL;
-		writer.write(windTurbineStatus, instanceHandle);
+		if (initialized) {
+			InstanceHandle_t instanceHandle = InstanceHandle_t.HANDLE_NIL;
+			writer.write(windTurbineStatus, instanceHandle);
+		}
+
+		else {
+			throw new IllegalStateException(
+					"Publisher hasn't been initialized. Please call the initialize method");
+		}
 	}
 }
